b6eca9ff2f1075ba7303970f1c4ebe3c5483fc37
Claudio Cicconetti <ccicconetti@mbigroup.it>
2023-01-30 11:03:27 +0100

    Add Mx2 ApplicationList messages

--- src/messages.rs
+++ src/messages.rs
@@ -4,6 +4,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use actix_web::App;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::fmt::{Display, Formatter};
@@ -11,6 +12,26 @@ use std::fmt::{Display, Formatter};
 /// Validate a message (or element thereof).
 pub trait Validate {
     fn validate(&self) -> Result<(), String>;
+}
+
+/// Return error if the vector of problems passed is not empty.
+fn check(problems: Vec<String>) -> Result<(), String> {
+    if problems.is_empty() {
+        Ok(())
+    } else {
+        Err(problems.join(";").to_string())
+    }
+}
+
+/// Add a problem to the list if validation fails.
+fn add_problem<T>(item: &T, problems: &mut Vec<String>)
+where
+    T: Validate,
+{
+    match item.validate() {
+        Ok(()) => (),
+        Err(err) => problems.push(err),
+    }
 }
 
 /// Polygon as defined in RFC 7946.
@@ -145,7 +166,7 @@ struct AppList {
 /// ApplicationList message used to retrieve the apps from the LCM proxy
 struct ApplicationList {
     /// List of user applications available to the device application.
-    appList: Vec<AppInfo>,
+    appList: Vec<AppList>,
 }
 
 impl Validate for Polygon {
@@ -201,6 +222,77 @@ impl Validate for LocationConstraints {
     }
 }
 
+impl Validate for AppCharcs {
+    fn validate(&self) -> Result<(), String> {
+        match &self.serviceCont {
+            Some(x) => match x {
+                0 | 1 => Ok(()),
+                other => Err(format!("invalid serviceCont value: {other}")),
+            },
+            None => Ok(()),
+        }
+    }
+}
+
+impl Validate for AppInfo {
+    fn validate(&self) -> Result<(), String> {
+        let mut problems: Vec<String> = vec![];
+        if self.appName.len() > 32 {
+            problems.push("appName is too long".to_string());
+        }
+        if self.appProvider.len() > 32 {
+            problems.push("appProvider is too long".to_string());
+        }
+        if self.appSoftVersion.len() > 32 {
+            problems.push("appSoftVersion is too long".to_string());
+        }
+        if self.appDescription.len() > 128 {
+            problems.push("appDescription is too long".to_string());
+        }
+        for c in &self.appLocation {
+            add_problem(c, &mut problems);
+        }
+        match &self.appCharcs {
+            Some(appCharcs) => add_problem(appCharcs, &mut problems),
+            None => (),
+        }
+
+        check(problems)
+    }
+}
+
+impl Validate for VendorSpecificExt {
+    fn validate(&self) -> Result<(), String> {
+        if self.vendorId.len() > 32 {
+            Err("vendorId is too long".to_string())
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl Validate for AppList {
+    fn validate(&self) -> Result<(), String> {
+        let mut problems: Vec<String> = vec![];
+        add_problem(&self.appInfo, &mut problems);
+        match &self.vendorSpecificExt {
+            Some(x) => add_problem(x, &mut problems),
+            None => (),
+        }
+        check(problems)
+    }
+}
+
+impl Validate for ApplicationList {
+    fn validate(&self) -> Result<(), String> {
+        let mut problems: Vec<String> = vec![];
+        for a in &self.appList {
+            add_problem(a, &mut problems);
+        }
+        check(problems)
+    }
+}
+
 impl Display for Polygon {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut areas: Vec<String> = vec![];
@@ -246,29 +338,81 @@ impl Display for LocationConstraints {
     }
 }
 
-// impl Display for AppList {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-//         write!(
-//             f,
-//             "appInfo:\n{}{}",
-//             self.appInfo,
-//             match self.vendorSpecificExt {
-//                 Some(x) => format!("vendorSpecificExt:\n{}", x.vendorId),
-//                 None => "".to_string(),
-//             }
-//         )
-//     }
-// }
+impl Display for AppCharcs {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "memory: {} MB, storage: {} MB, latency: {} ms, bandwidth: {} kb/s, continuity {}",
+            &self.memory.unwrap_or(0),
+            &self.storage.unwrap_or(0),
+            &self.latency.unwrap_or(0),
+            &self.bandwidth.unwrap_or(0),
+            match &self.serviceCont {
+                Some(x) => match x {
+                    0 => "not required",
+                    1 => "required",
+                    _other => "invalid value",
+                },
+                None => "not specified",
+            }
+        )
+    }
+}
 
-// impl Display for ApplicationList {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-//         let mut ret = String::from("");
-//         self.appList
-//             .iter()
-//             .for_each(|x| ret.push_str(format!("\n{}", x.to_string()).as_str()));
-//         write!(f, "{}", ret)
-//     }
-// }
+impl Display for AppInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let location_constraints: Vec<String> =
+            self.appLocation.iter().map(|x| x.to_string()).collect();
+        write!(f, "appDId: {}, appName: {}, appProvider: {}, appSoftVersion: {}, appDVersion: {}, appDescription: {}, appLocation: {}, appCharcs: {}",
+    self.appDId, self.appName, self.appProvider, self.appSoftVersion, self.appDVersion, self.appDescription,
+    location_constraints.join(","), match &self.appCharcs {
+        Some(x) => x.to_string(),
+        None => "unspecified".to_string(),
+   })
+    }
+}
+
+impl AppInfo {
+    fn empty() -> Self {
+        Self {
+            appDId: "".to_owned(),
+            appName: "".to_owned(),
+            appProvider: "".to_owned(),
+            appSoftVersion: "".to_owned(),
+            appDVersion: "".to_owned(),
+            appDescription: "".to_owned(),
+            appLocation: vec![],
+            appCharcs: None,
+        }
+    }
+}
+
+impl Display for VendorSpecificExt {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "vendorId: {}", self.vendorId)
+    }
+}
+
+impl Display for AppList {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "appInfo: {}{}",
+            self.appInfo,
+            match &self.vendorSpecificExt {
+                Some(x) => format!(", vendorSpecificExt: {}", x.vendorId),
+                None => "".to_string(),
+            }
+        )
+    }
+}
+
+impl Display for ApplicationList {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let apps: Vec<String> = self.appList.iter().map(|x| x.to_string()).collect();
+        write!(f, "{}", apps.join("\n"))
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -280,6 +424,33 @@ mod tests {
                 vec![vec![0.0, 1.0], vec![1.0, 1.0], vec![1.0, 0.0]],
                 vec![vec![0.0, 0.1], vec![0.1, 0.1], vec![0.1, 0.0]],
             ],
+        }
+    }
+
+    fn default_app_charcs() -> AppCharcs {
+        AppCharcs {
+            memory: Some(100),
+            storage: Some(200),
+            latency: Some(50),
+            bandwidth: Some(42),
+            serviceCont: Some(1),
+        }
+    }
+
+    fn default_app_info() -> AppInfo {
+        AppInfo {
+            appDId: "test_appDId".to_owned(),
+            appName: "test_appName".to_owned(),
+            appProvider: "test_appProvider".to_owned(),
+            appSoftVersion: "test_appSoftVersion".to_owned(),
+            appDVersion: "test_appDVersion".to_owned(),
+            appDescription: "test_appDescription".to_owned(),
+            appLocation: vec![LocationConstraints {
+                countryCode: None,
+                civicAddressElement: vec![],
+                area: Some(default_polygon()),
+            }],
+            appCharcs: Some(default_app_charcs()),
         }
     }
 
@@ -327,6 +498,80 @@ mod tests {
         c.civicAddressElement.clear();
         assert_eq!(Ok(()), c.validate());
         println!("{}", c);
+    }
+
+    #[test]
+    fn test_message_app_charcs() {
+        let a = AppCharcs {
+            memory: None,
+            storage: None,
+            latency: None,
+            bandwidth: None,
+            serviceCont: None,
+        };
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
+
+        let mut a = default_app_charcs();
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
+
+        a.serviceCont = Some(0);
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
+
+        a.serviceCont = Some(2);
+        assert!(a.validate().is_err());
+    }
+
+    #[test]
+    fn test_message_app_info() {
+        let a = AppInfo::empty();
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
+
+        let mut a = default_app_info();
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
+
+        let mut long = "".to_string();
+        (0..33).for_each(|_| long.push('a'));
+        a.appName = long;
+        assert!(a.validate().is_err());
+    }
+
+    #[test]
+    fn test_message_vendor_specific_ext() {
+        let mut v = VendorSpecificExt {
+            vendorId: "specific-extension".to_owned(),
+        };
+        assert_eq!(Ok(()), v.validate());
+        println!("{}", v);
+
+        let mut long = "".to_string();
+        (0..33).for_each(|_| long.push('a'));
+        v.vendorId = long;
+        assert!(v.validate().is_err());
+    }
+
+    #[test]
+    fn test_message_application_list() {
+        let a = ApplicationList {
+            appList: vec![
+                AppList {
+                    appInfo: default_app_info(),
+                    vendorSpecificExt: None,
+                },
+                AppList {
+                    appInfo: AppInfo::empty(),
+                    vendorSpecificExt: Some(VendorSpecificExt {
+                        vendorId: "vendor-specific".to_string(),
+                    }),
+                },
+            ],
+        };
+        assert_eq!(Ok(()), a.validate());
+        println!("{}", a);
     }
 
     #[test]
